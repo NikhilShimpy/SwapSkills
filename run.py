6d671935f188cc7d9c2edb3e92f831abd76d742e
NikhilShimpy <nikhilshimpy712@gmail.com>
2025-10-04 00:24:14 +0530

    run file modified

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 from app import create_app
 
-app = create_app()
+app = create_app()   # Flask app must be exposed as `app`
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
+if __name__ == "__main__":
+    app.run()
 
